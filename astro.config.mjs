51293e4f2e6f06faf04dfb82bc211a7996c1e810
Kamil M <116840531+proxima812@users.noreply.github.com>
2024-12-24 19:13:50 +0500

    1

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,13 +11,4 @@ export default defineConfig({
 	output: "server",
 	integrations: [react(), tailwind()],
 	adapter: vercel({ isr: { expiration: 60 * 60 * 24 }, maxDuration: 60 }),
-	// env: {
-	// 	schema: {
-	// 		SUPABASE_URL: envField.string({
-	// 			context: "server",
-	// 			access: "public",
-	// 		}),
-	// 		SUPABASE_ANON_KEY: envField.string({ context: "server", access: "secret" }),
-	// 	},
-	// },
 })
